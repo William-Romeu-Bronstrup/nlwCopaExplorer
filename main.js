402c41a3954cf859b0989b29954597c022727e3f
William <williambronstrup13@gmail.com>
2022-11-04 17:56:51 -0300

    Finalizando o Projeto da Rocketseat
    Foi feito uma nova Feature de trocar as cores do projeto.

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+let body = document.getElementById("body");
+
+let backgroundColors = ["yellow", "blue", "green"];
+
+let change = 1;
+
+function changeBackground() {
+
+  switch (change) {
+  
+    case 1:
+      body.classList.remove(backgroundColors[0]);
+      body.classList.add(backgroundColors[1]);
+
+      change += 1;
+      break;
+
+    case 2:
+      body.classList.remove(backgroundColors[1]);
+      body.classList.add(backgroundColors[2]);
+
+      change += 1;
+      break;
+
+    default:
+      body.classList.remove(backgroundColors[2]);
+      body.classList.add(backgroundColors[0]);
+
+      change = 1;
+  }
+
+}
+
+
 function createGame(player1, hour, player2) {
   return `
       <li>
@@ -8,9 +42,12 @@ function createGame(player1, hour, player2) {
   `;
 }
 
+let delay = 0;
 function createCard(date, day, games) {
+  0
+  delay += 0.2;
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
 
       <ul>
@@ -21,28 +58,20 @@ function createCard(date, day, games) {
   `;
 }
 
-document.querySelector("#app").innerHTML = `
-      <header>
-        <img src="./assets/logo.svg" alt="NLW Copa Logo" />
-      </header>
+document.querySelector("#cards").innerHTML =
 
-      <main id="cards">
-
-        ${createCard(
-          '23/11', 'Quarta', 
-          createGame("morocco", "07:00" , "croatia") +
-          createGame("germany", "10:00" , "japan") +
-          createGame("spain", "13:00" , "costaRica") +
-          createGame("belgium", "16:00" , "canada")
-        )}
-
-        ${createCard(
-          '24/11', 'Quinta', 
-          createGame("switzerland", "07:00" , "cameroon") +
-          createGame("uruguay", "10:00" , "southKorea") +
-          createGame("portugal", "13:00" , "ghana") +
-          createGame("brazil", "16:00" , "serbia")
-        )}
-
-      </main>
-`;
+    createCard(
+      '23/11', 'Quarta', 
+      createGame("morocco", "07:00" , "croatia") +
+      createGame("germany", "10:00" , "japan") +
+      createGame("spain", "13:00" , "costaRica") +
+      createGame("belgium", "16:00" , "canada")
+    )
+    +
+    createCard(
+      '24/11', 'Quinta', 
+      createGame("switzerland", "07:00" , "cameroon") +
+      createGame("uruguay", "10:00" , "southKorea") +
+      createGame("portugal", "13:00" , "ghana") +
+      createGame("brazil", "16:00" , "serbia")
+    );
